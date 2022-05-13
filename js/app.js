f3cb0c12829562664ffaf5651c094c15ed13db56
Dmitriy <104874928+DmitriyGreznev@users.noreply.github.com>
2022-05-13 20:56:45 +0300

    Done!

---
+++ js/app.js
@@ -0,0 +1,146 @@
+$(function() {
+
+
+
+  /* Main Slider */
+
+  $('#workSlider').slick({
+    speed: 1000,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: false,
+    dots: false,
+    variableWidth: true,
+
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+});
+
+  $("#slickPrevious").on("click", function(event) {
+  event.preventDefault();
+
+  let currentSlider = $(this).parents('.portfolio__inner').find('[data-slider="workSlider"]');
+
+  currentSlider.slick("slickPrev");
+  });
+
+  $("#slickNext").on("click", function(event) {
+  event.preventDefault();
+
+  let currentSlider = $(this).parents('.portfolio__inner').find('[data-slider="workSlider"]');
+
+  currentSlider.slick("slickNext");
+  });
+
+
+  /* Modal */
+
+  const modalCall = $("[data-modal]");
+  const modalClose = $("[data-close]");
+
+  modalCall.on("click", function(event){
+      event.preventDefault();
+      let $this = $(this);
+      let modalId = $this.data('modal')
+
+      $(modalId).addClass('show');
+      $("body").addClass('no-scroll');
+
+      setTimeout(function() {
+          $(modalId).find('.modal__inner').css({
+              transform: "rotateX(0)"
+          });
+      }, 200);
+
+      const modalSlider = $('[data-slider="modalSlider"]');
+
+      modalSlider.slick('setPosition');
+  });
+
+
+  modalClose.on("click", function(event){
+      event.preventDefault();
+      let $this = $(this);
+      let modalParent = $this.parents('.modal');
+      modalParent.find(".modal__inner").css({
+          transform: "rotateX(90deg)"
+      })
+
+      setTimeout(function(){
+          modalParent.removeClass('show');
+          $("body").removeClass('no-scroll');
+      }, 200);
+  });
+
+  $(".modal").on("click", function(event){
+      let $this = $(this);
+          $this.find('.modal__inner').css({
+          transform: "rotateX(90deg)"
+      });
+
+      setTimeout(function() {
+          $this.removeClass('show');
+          $("body").removeClass('no-scroll');
+      }, 200);
+  });
+
+      $(".modal__inner").on("click", function(event){
+          event.stopPropagation();
+      });
+
+
+
+  /* Modal Slider */
+
+  $('#modalSlider').slick({
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+
+});
+
+  $("#modalSlickPrevious").on("click", function(event) {
+  event.preventDefault();
+
+  let currentModalSlider = $(this).parents('.modal__work__prev').find('[data-slider="modalSlider"]');
+
+  currentModalSlider.slick("slickPrev");
+  });
+
+  $("#modalSlickNext").on("click", function(event) {
+  event.preventDefault();
+
+  let currentModalSlider = $(this).parents('.modal__work__prev').find('[data-slider="modalSlider"]');
+
+  currentModalSlider.slick("slickNext");
+  });
+
+});
